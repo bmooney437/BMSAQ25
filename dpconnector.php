67e9282c6c10346caac9a6589f7f46cd50149bfd
bmooney437 <bmooney437@icloud.com>
2025-04-03 02:39:53 +0100

    updated dpconector

--- dpconnector.php
+++ dpconnector.php
@@ -1,15 +1,13 @@
 <?php
 // dpconnector.php
-
-$servername   = "localhost";      // Use "localhost" or "127.0.0.1"
-$db_username  = "root";           // MAMP username
-$db_password  = "root";           // MAMP password
-$dbname       = "bmsaq";          // Your database name
-$port         = 8889;             // MAMP default port
-$socket       = "/Applications/MAMP/tmp/mysql/mysql.sock"; // MAMP socket
+$servername   = "localhost"; 
+$db_username  = "root";
+$db_password  = "root";
+$dbname       = "bmsaq";
+$port         = 8889;
+$socket       = "/Applications/MAMP/tmp/mysql/mysql.sock";
 
 $conn = new mysqli($servername, $db_username, $db_password, $dbname, $port, $socket);
-
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
